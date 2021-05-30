e34258d775147375e4cef37e703ed5fb7aec11dc
Dias <lord.taiko>
2021-05-30 19:59:05 +0600

    [ADD]

---
+++ main.py
@@ -0,0 +1,114 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.properties import ObjectProperty
+from kivy.core.window import Window
+# from kivy.network.urlrequest import UrlRequest
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy_garden.mapview import MapView
+
+import requests
+import json
+
+from kivy.config import Config
+Config.set('kivy', 'keyboard_mode', 'systemanddock')
+
+
+Window.size = (480, 853)
+
+
+from kivymd.app import MDApp
+
+
+class FirstWindow(Screen):
+    pass
+
+
+class SecondWindow(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+class LocationMap(MapView):
+    pass
+
+
+def check_number(number):
+    with open('data.json', 'r') as file:
+        data = file.read()
+        if data == '':
+            return True
+        else:
+            data = json.loads(data)
+            if str(data['number']) == str(number):
+                return True
+            else:
+                return False
+
+
+def get_data(number):
+    if len(number) != 11 or number == '':
+        return {
+            "ip": 'ERROR',
+            "city": 'INCORRECT',
+            "region": 'NUMBER',
+            "country": 'PLEASE',
+            "loc": 'TRY',
+            "org": 'ANOTHER',
+            "timezone": 'PHONE',
+            "readme": 'NUMBER'
+        }
+
+    if check_number(number):
+        with open('data.json', 'w') as f:
+            url = 'http://ipinfo.io'
+            response = requests.get(url).json()
+            response['number'] = number
+            json.dump(response, f, ensure_ascii=False)
+        return response
+    else:
+        return {
+            "ip": 'EXIST',
+            "city": 'PHONE',
+            "region": 'NUMBER',
+            "country": 'PLEASE',
+            "loc": 'TRY',
+            "org": 'ANOTHER',
+            "timezone": 'PHONE',
+            "readme": 'NUMBER'
+        }
+
+
+class Container(GridLayout):
+
+    def calculate(self):
+        try:
+            number = self.text_input.text
+        except:
+            number = ''
+
+        data = get_data(number)
+
+        self.ip.text = data['ip']
+        self.city.text = data['city']
+        self.reg.text = data['region']
+        self.country.text = data['country']
+        self.loc.text = data['loc']
+        self.host.text = data['org']
+        self.tzone.text = data['timezone']
+
+
+class MyApp(MDApp):
+    def __init__(self, **kwargs):
+        self.theme_cls.theme_style = "Dark"
+        super().__init__(**kwargs)
+
+    def build(self):
+        return Container()
+
+
+if __name__ == '__main__':
+    MyApp().run()
